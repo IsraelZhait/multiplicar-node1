3fc75d253edb1bf3df2571c3c5ac73cdb9de84ef
Israel Zahitsef <isrraelhuaman@gmail.com>
2020-06-11 18:51:35 -0500

    Comir inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,68 @@
+
+
+
+const fs = require('fs');
+const colors = require('colors');
+
+
+
+
+let listarTabla =(base, limite=10) =>{
+
+  console.log('=================='.green);
+  console.log(`======Tabla de ${base}===========`.blue);
+  console.log('=================='.white);
+  
+
+  for (let i = 1; i < limite ; i++) {
+  
+    console.log(` ${base}  * ${i} =${base* i} `);
+
+
+  }
+
+}
+
+
+
+
+
+let crearArchivo = (base , limite = 10) => {
+
+    return new Promise( ( resolve, reject ) => {
+
+      if(!Number(base)){
+        reject(` El valor introducido ''${base}'' no es un numero`);
+        return;
+      }
+
+      let data = '';
+
+      for (let i = 1; i < limite ; i++) {
+  
+        data +=(` ${base}  * ${i} =${base* i} \n`);
+  
+
+      }
+
+
+      fs.writeFile(`tablas/tabla-${base} -al-${limite}.txt`, data, (err) => {
+
+
+        if (err)
+             reject(err)
+         else 
+            resolve(`tabla-${base}-a;-${limite} .txt`);
+        
+      });
+
+          });
+}
+
+
+
+module.exports = {
+  crearArchivo,
+  listarTabla
+}
+
